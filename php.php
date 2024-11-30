eed78c6ce1527b02e19e7458082fb4e213112300
M2ryamal7asan <mariamal7asan@gmail.com>
2024-11-30 19:13:37 +0300

    fixing to have a correct result

--- php.php
+++ php.php
@@ -3,30 +3,34 @@
 //Data Retrieval//
 $url="https://data.gov.bh/explore/dataset/01-statistics-of-students-nationalities_updated/table/?disjunctive.year&disjunctive.semester&disjunctive.the_programs&sort=number_of_students";
 $response=file_get_contents($url);
-echo $response;
+
 $Sdata=json_decode($response,true);
-echo $response;
 
 // cheacking,  for errors when its accure//
-if (!$Sdata|| !isset($Sdata["results"])){
+if (!$Sdata|| !isset($Sdata["records"])){
 
     die("error fetching the data..")
 }
 
-$records = $Sdata["results"];
-print_r($records)
+$data = $Sdata["records"]; 
 
-//having the info for the students //
-   foreach($records as $student){
 
-          echo $student["Year"];
-          echo$student["Semester"];
-          echo$student["The Programs"];
-          echo$student["Nationality"];
-          echo$student["Colleges"];
-          echo$student["Number of Students"];
+//having the Data and information about the  students //
+
+   foreach($data as $student){
+
+        $Year[]= $student["Year"];
+          $Semester[]=$student["Semester"];
+            $The_Program[]= $student["The Program"];
+               $Nationality[]=$student["Nationality"];
+                 $Colleges[]=$student["Colleges"];
+                    $Number_of_Students[]=$student["Number of Students"];
 
 }
 
 
 ?>
+
+
+
+
